e829e005ae60931be2ea22915826f0f2e6f7c47f
Parkgeum <rmawjddd@gmail.com>
2019-06-11 14:52:33 +0900

    데이터 수정

--- routes/data_form.js
+++ routes/data_form.js
@@ -1,21 +1,23 @@
 var mongoose = require('mongoose');
 
 var beaconSchema = new mongoose.Schema({
-  "timestamp":String,
   "type":String,
   "mac":String,
-  "bleName":String,
-  "rssi":String,
-  "ibeaconUuid": String,
-  "ibeaconMajor": String,
-  "ibeaconMinor": String,
-  "ibeaconTxPower": String,
-  "battery": String,
-  "rawData":String,
-  "gatewayFree": String,
-  "gatewayLoad": String,
-  "temperature":String,
-  "humidity":String
+  //"data":[{
+    "timestamp":String,
+    "bleName":String,
+    "rssi":String,
+    "ibeaconUuid": String,
+    "ibeaconMajor": String,
+    "ibeaconMinor": String,
+    "ibeaconTxPower": String,
+    "battery": String,
+    "rawData":String,
+    "gatewayFree": String,
+    "gatewayLoad": String,
+    "temperature":String,
+    "humidity":String
+  //}]
 })
 
 module.exports = mongoose.model('beacon',beaconSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var uuid = require('uuid');
+var id = uuid.v4();
 var mongoose = require('mongoose');
 var DataSchema = require('./data_form.js');
 mongoose.connect('mongodb://localhost:27017/Smart', { useNewUrlParser: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(id);
 });
 
 router.post('/insert', function(req, res, next) {
@@ -15,7 +17,28 @@ router.post('/insert', function(req, res, next) {
   var jsoncontent = eval(New_Data);//jsoncontent[0]...
   console.log(Object.keys(jsoncontent).length);
   //res.send(jsoncontent.type);
+/*
+  for (var i=0; i<Object.keys(jsoncontent).length; i++){
+    var schema = new DataSchema();
+    schema.data = jsoncontent[i];
+    schema.type = jsoncontent[i].type;
+    schema.mac = jsoncontent[i].mac;
 
+    console.log(schema.mac+"-------------");
+
+    DataSchema.findOne({'mac':schema.mac}, function(err, datahistory){
+      //비콘을 처음 발견한 경우 데이터베이스 새로 생성
+      if (datahistory==null) DataSchema.create(schema, function() {console.log("생성");});
+       //기존에 데이터가 존재하는 경우
+      else {
+        var DATA = datahistory.data;
+        DATA.push(schema.data)
+
+        datahistory.update({'data':DATA}, function() {console.log("추가");});
+      }
+    })
+  }
+  */
   for (var i=0; i<Object.keys(jsoncontent).length; i++){
     DataSchema.create(jsoncontent[i], function() {});
   }
